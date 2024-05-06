dfbc1ae6048f504a5dbf77a6eea590f519da9473
alexey <RepelLeha@yandex.ru>
2024-05-06 20:15:30 +0300

    Added credits

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -20,10 +20,13 @@ func _on_StartButton_pressed():
 	get_tree().change_scene_to_file("res://scenes/CharacterGenerator.tscn")
 	
 
-
 func _on_ExitButton_pressed():
 	get_tree().quit()
 
 
 func _on_options_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
